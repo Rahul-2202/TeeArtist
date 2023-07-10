fe3e965a38238d65a7248c8826cf468b19d9aba4
Rahul-2202 <rahulmanglani0@gmail.com>
2023-07-10 16:51:09 +0530

    final card and rendering cards on products page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import ProductPage from "./pages/ProductPage";
 // import Contact from "./Contact";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route exact path="/product/:prodId" element={<ProductPage />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,13 +1,15 @@
 import React, {useState} from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-const ProductCard = ({image, title, description, price}) => {
+import {Link} from "react-router-dom";
+const ProductCard = ({prodId, image, title, description, price}) => {
   const [isLiked, setIsLiked] = useState(false);
   const handleLike = () => {
     setIsLiked(!isLiked);
     console.log(title);
+    console.log(prodId);
   };
   return (
     <Card sx={{maxWidth: 245, maxHeight: 385, margin: 4}}>
@@ -19,60 +21,70 @@ const ProductCard = ({image, title, description, price}) => {
             right: 10,
             cursor: "pointer"
           }}
-          color={isLiked ? "disabled" : "error"}
+          color={isLiked ? "error" : "disabled"}
           className="wishlistIcon"
           onClick={handleLike}
         />
-        <CardMedia
-          component="img"
-          height={307}
-          maxWidth={245}
-          alt="product image"
-          image={image}
-        />
-      </div>
-      <div style={{padding: "0px 5px 0px 12px"}}>
-        <h4
-          className="text-left"
-          style={{
-            color: "rgb(88, 92, 112)",
-            padding: 0,
-            margin: 0,
-            marginBottom: 4,
-            marginTop: 7
-          }}
+        <Link
+          to={`/product/${prodId}`}
+          style={{color: "inherit", textDecoration: "none"}}
         >
-          {title}
-        </h4>
-        <hr
-          style={{
-            padding: 0,
-            margin: 0,
-            marginBottom: 1,
-            marginTop: 1
-          }}
-        />
-        <div className="row">
-          <h5
+          <CardMedia
+            component="img"
+            height={307}
+            maxWidth={245}
+            alt="product image"
+            image={image}
+          />
+        </Link>
+      </div>
+      <Link
+        to={`/product/${prodId}`}
+        style={{color: "inherit", textDecoration: "none"}}
+      >
+        <div style={{padding: "0px 5px 0px 12px"}}>
+          <h4
+            className="text-left"
             style={{
+              color: "rgb(88, 92, 112)",
               padding: 0,
               margin: 0,
               marginBottom: 4,
-              fontWeight: "normal"
+              marginTop: 7
             }}
           >
-            {description}
-          </h5>
-          <div className="col-12" style={{paddingRight: "0px"}}>
-            <span className="offer fsemibold">
-              <b>₹ {price}</b>
-            </span>
-          </div>
-          <div className="col-12 percentage-off member-savings pb-1">
-            &nbsp;
+            {title}
+          </h4>
+          <hr
+            style={{
+              padding: 0,
+              margin: 0,
+              marginBottom: 1,
+              marginTop: 1
+            }}
+          />
+          <div className="row">
+            <h5
+              style={{
+                padding: 0,
+                margin: 0,
+                marginBottom: 4,
+                fontWeight: "normal"
+              }}
+            >
+              {description}
+            </h5>
+            <div className="col-12" style={{paddingRight: "0px"}}>
+              <span className="offer fsemibold">
+                <b>₹ {price}</b>
+              </span>
+            </div>
+            <div className="col-12 percentage-off member-savings pb-1">
+              &nbsp;
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </Card>
   );
 };
